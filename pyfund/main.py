dc165e25eb1e7332f063c57ffd1f778da6a89159
Dawid Gerstel <dawidgerstel@gmail.com>
2022-06-18 14:43:17 +0200

    Add purchase history; refactor tests

--- pyfund/main.py
+++ pyfund/main.py
@@ -1,7 +1,7 @@
 import datetime
 from dataclasses import dataclass
 from enum import Enum
-from typing import Optional
+from typing import Optional, Sequence
 
 
 class AssetEnum(Enum):
@@ -62,12 +62,28 @@ class Purchase:
                + f"using your {self.account.value} account."
 
 
+@dataclass
+class History:
+    """History is a list of purchases."""
+    purchase_arr: Sequence[Purchase]
+
+    def __repr__(self):
+        return "\n".join([p.__repr__() for p in self.purchase_arr])
+
+    def total_cost(self):
+        return sum(p.price for p in self.purchase_arr)
+
+
 def main():
     p = Purchase(AssetEnum.GOLD, 1734.34, Account.UNKNOWN)
     print(p)
 
     p2 = Purchase(AssetEnum.SILVER, 123.34, Account.SUPERBROKER)
     print(p2)
+
+    h = History([p, p2])
+    print(h)
+    print(h.total_cost())
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,21 @@
-from pyfund.main import Purchase, AssetEnum, Account
+from pyfund.main import Purchase, AssetEnum, Account, History
 import pytest
 
 
-@pytest.mark.parametrize(
-    "asset_enum,price,account_enum,expected_str",
-    [
-        (AssetEnum.GOLD, 1734.34, Account.UNKNOWN,
-         "On 2022-06-18 you bought 1.0 oz of gold for 1734.34 EUR using your UNKNOWN account."),
-        (AssetEnum.SILVER, 123.34, Account.SUPERBROKER,
-         "On 2022-06-18 you bought 1.0 oz of silver for 123.34 EUR using your SuperBroker account."),
+@pytest.fixture
+def purchases():
+    return [
+        Purchase(AssetEnum.GOLD, 1734.34, Account.UNKNOWN),
+        Purchase(AssetEnum.SILVER, 123.34, Account.SUPERBROKER),
     ]
-)
-def test_purchase(asset_enum, price, account_enum, expected_str):
-    p = Purchase(asset_enum, price, account_enum)
-    assert p.__repr__() == expected_str
+
+
+def test_purchase(purchases):
+    assert purchases[0].__repr__() == "On 2022-06-18 you bought 1.0 oz of gold for 1734.34 EUR using your UNKNOWN account."
+    assert purchases[1].__repr__() == "On 2022-06-18 you bought 1.0 oz of silver for 123.34 EUR using your SuperBroker account."
+
+
+def test_history(purchases):
+    h = History(purchases)
+    assert h.__repr__() == "On 2022-06-18 you bought 1.0 oz of gold for 1734.34 EUR using your UNKNOWN account."\
+                            + "\n" + "On 2022-06-18 you bought 1.0 oz of silver for 123.34 EUR using your SuperBroker account."
