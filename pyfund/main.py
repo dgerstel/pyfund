e2654a61bfece692b0b5702104f9aa4e5a23cba9
Dawid Gerstel <dawidgerstel@gmail.com>
2022-06-18 14:22:53 +0200

    Implement purchasing process

--- pyfund/main.py
+++ pyfund/main.py
@@ -1,6 +1,74 @@
-def dummy():
-    pass
+import datetime
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+
+
+class AssetEnum(Enum):
+    GOLD = "gold"
+    SILVER = "silver"
+
+
+class Unit(Enum):
+    OZ = "oz"
+
+
+ASSET_AND_UNIT = {
+    AssetEnum.GOLD: Unit.OZ,
+    AssetEnum.SILVER: Unit.OZ,
+}
+
+
+@dataclass
+class Asset:
+    """Asset with its unit and current price."""
+    name: AssetEnum
+    current_price: Optional[int] = None
+
+    def __post_init__(self):
+        self.unit = ASSET_AND_UNIT[self.name]
+
+    def __repr__(self):
+        return self.name.value
+
+
+class Account(Enum):
+    UNKNOWN = "UNKNOWN"
+    SUPERBROKER = "SuperBroker"
+    DEGIRO = "DEGIRO"
+
+
+class Currency(Enum):
+    EUR = "EUR"
+    USD = "usd"
+    PLN = "pln"
+
+
+@dataclass
+class Purchase:
+    """Represents a purchase of an asset."""
+    asset_enum: AssetEnum
+    price: float
+    account: Account = Account.UNKNOWN
+    qty: float = 1.0
+    date: datetime.datetime = datetime.date.today()
+    currency: Currency = Currency.EUR
+
+    def __post_init__(self):
+        self.asset: Asset = Asset(self.asset_enum)
+
+    def __repr__(self):
+        return f"On {self.date} you bought {self.qty} {self.asset.unit.value} of {self.asset} for {self.price} {self.currency.value} "\
+               + f"using your {self.account.value} account."
+
+
+def main():
+    p = Purchase(AssetEnum.GOLD, 1734.34, Account.UNKNOWN)
+    print(p)
+
+    p2 = Purchase(AssetEnum.SILVER, 123.34, Account.SUPERBROKER)
+    print(p2)
 
 
 if __name__ == "__main__":
-    pass
+    main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,16 @@
-from pyfund.main import dummy
+from pyfund.main import Purchase, AssetEnum, Account
+import pytest
 
 
-def test_dummy():
-    dummy()
+@pytest.mark.parametrize(
+    "asset_enum,price,account_enum,expected_str",
+    [
+        (AssetEnum.GOLD, 1734.34, Account.UNKNOWN,
+         "On 2022-06-18 you bought 1.0 oz of gold for 1734.34 EUR using your UNKNOWN account."),
+        (AssetEnum.SILVER, 123.34, Account.SUPERBROKER,
+         "On 2022-06-18 you bought 1.0 oz of silver for 123.34 EUR using your SuperBroker account."),
+    ]
+)
+def test_purchase(asset_enum, price, account_enum, expected_str):
+    p = Purchase(asset_enum, price, account_enum)
+    assert p.__repr__() == expected_str
